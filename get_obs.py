24bc7267977781b178d97701b2880c0008e44db5
SN-Liu <124896978+SN-Liu@users.noreply.github.com>
2023-06-09 18:13:02 +0800

    Delete get_obs.py

--- get_obs.py
+++
@@ -1,9 +0,0 @@
-def get_obs(x_data,y_data):
-    stable_segment = []
-    increase_segment = []
-    for i in range(len(x_data-1)):
-        if abs(x_data[i] - x_data[i+1]) < 0.01:
-            stable_segment.append(x_data[i],x_data[i+1])
-        elif abs(x_data[i]-x_data[i+1]) > 0.05:
-            increase_segment.append(x_data[i])
-            
